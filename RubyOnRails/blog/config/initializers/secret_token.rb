ebdada23e4e6cf14c5b730703325f2cef97fdaec
suresundar <suresundar@gmail.com>
2012-02-23 22:44:57 +0530

    Commit@2-23 22:45

--- RubyOnRails/blog/config/initializers/secret_token.rb
+++ RubyOnRails/blog/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '9cfb2b6c8b06a204a2f778682012c973fc3e7f089a3b6ae8d55eb9842d36342fd6c8526904e97190c688febef55f29b6449fc67291487425caf6700fc3eb1c1d'
+Blog::Application.config.secret_token = '934e84c35149f71f6272a4ce50a803c96ce0798c05f958d6715e1eddd415e55d2291397ebe2dfbf976b0e4fd23f11f48744af0c01d2ad031dfc9d57bac60a8c4'
--- RubyOnRails/blog/config/routes.rb
+++ RubyOnRails/blog/config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,11 +50,15 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  resources :users, :user_sessions
+  match 'login' => 'user_sessions#new', :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
+  root :to => 'user_sessions#new', :as => :login
 end
