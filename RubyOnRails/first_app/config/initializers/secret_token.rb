bf2371d1e27add64ff259aaba8701a2f0768dfc8
Sundararaman Sethuraman <suresundar@gmail.com>
2012-03-02 21:27:07 +0530

    modified:   blog/README 	deleted:    blog/public/index.html 	deleted:    blog/public/index.html.old 	deleted:    first_app/.gitignore 	deleted:    first_app/Gemfile 	deleted:    first_app/Gemfile.bak 	deleted:    first_app/Gemfile.lock 	deleted:    first_app/README 	deleted:    first_app/Rakefile 	deleted:    first_app/app/controllers/application_controller.rb 	deleted:    first_app/app/controllers/users_controller.rb 	deleted:    first_app/app/controllers/users_controller.rb.bak 	deleted:    first_app/app/helpers/application_helper.rb 	deleted:    first_app/app/helpers/users_helper.rb 	deleted:    first_app/app/models/user.rb 	deleted:    first_app/app/views/layouts/application.html.erb 	deleted:    first_app/app/views/layouts/application.html.erb.bak 	deleted:    first_app/app/views/users/_form.html.erb 	deleted:    first_app/app/views/users/_form.html.erb.bak 	deleted:    first_app/app/views/users/edit.html.erb 	deleted:    first_app/app/views/users/index.html.erb 	deleted:    first_app/app/views/users/index.html.erb.bak 	deleted:    first_app/app/views/users/new.html.erb 	deleted:    first_app/app/views/users/show.html.erb 	deleted:    first_app/config.ru 	deleted:    first_app/config/application.rb 	deleted:    first_app/config/boot.rb 	deleted:    first_app/config/database.yml 	deleted:    first_app/config/environment.rb 	deleted:    first_app/config/environments/development.rb 	deleted:    first_app/config/environments/production.rb 	deleted:    first_app/config/environments/test.rb         deleted:    first_app/config/initializers/backtrace_silencers.rb 	deleted:    first_app/config/initializers/inflections.rb 	deleted:    first_app/config/initializers/mime_types.rb 	deleted:    first_app/config/initializers/secret_token.rb 	deleted:    first_app/config/initializers/session_store.rb 	deleted:    first_app/config/locales/en.yml 	deleted:    first_app/config/routes.rb 	deleted:    first_app/db/migrate/20120227142032_create_users.rb 	deleted:    first_app/db/seeds.rb 	deleted:    first_app/doc/README_FOR_APP 	deleted:    first_app/lib/tasks/.gitkeep 	deleted:    first_app/public/404.html 	deleted:    first_app/public/422.html 	deleted:    first_app/public/500.html 	deleted:    first_app/public/favicon.ico 	deleted:    first_app/public/images/rails.png 	deleted:    first_app/public/index.html 	deleted:    first_app/public/javascripts/application.js 	deleted:    first_app/public/javascripts/controls.js 	deleted:    first_app/public/javascripts/dragdrop.js 	deleted:    first_app/public/javascripts/effects.js 	deleted:    first_app/public/javascripts/prototype.js 	deleted:    first_app/public/javascripts/rails.js 	deleted:    first_app/public/robots.txt 	deleted:    first_app/public/stylesheets/.gitkeep 	deleted:    first_app/public/stylesheets/scaffold.css 	deleted:    first_app/script/rails 	deleted:    first_app/test/fixtures/users.yml 	deleted:    first_app/test/functional/users_controller_test.rb 	deleted:    first_app/test/performance/browsing_test.rb 	deleted:    first_app/test/test_helper.rb 	deleted:    first_app/test/unit/helpers/users_helper_test.rb 	deleted:    first_app/test/unit/user_test.rb 	deleted:    first_app/vendor/plugins/.gitkeep

--- RubyOnRails/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '77f2439a79f4320b9499b2f1872df1248dd7f3f5d14fc5c32c133ad0145ac8ac14c188c7b1027a4d4d7a4d9543044702b8938e01edc236bf8f19883477237d7f'
